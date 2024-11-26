5a2dc46b38eeb22ffbd9b2d767fe631811b94e04
Matog1 <84256186+Matog1@users.noreply.github.com>
2024-11-26 01:52:42 -0300

    Update Imovel_Match.java

--- src/main/java/org/example/Imovel_Match.java
+++ src/main/java/org/example/Imovel_Match.java
@@ -2,20 +2,27 @@ package org.example;
 
 public class Imovel_Match {
 
-	public double quadrado(double lado) {
-	      return lado * lado;
-	   }
+    public double calcularPrecoPorMetroQuadrado(double preco, double area) {
+        return preco / area;
+    }
 
-	   public double retangulo(double base, double altura) {
-	      return base * altura;
-	   }
+    public boolean compararPrecos(double preco1, double preco2) {
+        return preco1 < preco2; // Retorna true se o primeiro imóvel é mais barato que o segundo
+    }
 
-	   public double circulo(double raio) {
-	      return 3.1415 * raio * raio;
-	   }
+    public double calcularArea(double lado) {
+        return lado * lado;
+    }
 
-	   public double trianguloEquilatero(double base, double altura) {
-	      return base * altura / 2.0;
-	   }
-	
+    public double calcularAreaRetangulo(double base, double altura) {
+        return base * altura;
+    }
+
+    public double calcularAreaCirculo(double raio) {
+        return 3.1415 * raio * raio;
+    }
+
+    public double calcularAreaTriangulo(double base, double altura) {
+        return base * altura / 2.0;
+    }
 }
